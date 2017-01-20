94758579584a4f70b95da30de5888ee6cbf647e9
MKRetunz <Martijnklok@hotmail.nl>
2017-01-20 09:35:09 +0100

    Inserted base values for every class, these are temporary and need balancing

--- start/classstats.cpp
+++ start/classstats.cpp
@@ -21,21 +21,75 @@ ClassStats::~ClassStats()
 int ClassStats::GetStat(int stat, int baseclass)
 {
 	if (stat = 0) {
+		if (baseclass = 0) {
+			HPAllocater = 24;
+		}
+		if (baseclass = 1) {
+			HPAllocater = 19;
+		}
+		if (baseclass = 2) {
+			HPAllocater = 30;
+		}
 		return HPAllocater;
 	}
 	else if (stat = 1) {
+		if (baseclass = 0) {
+			DamAllocater = 21;
+		}
+		if (baseclass = 1) {
+			DamAllocater = 19;
+		}
+		if (baseclass = 2) {
+			DamAllocater = 28;
+		}
 		return DamAllocater;
 	}
 	else if (stat = 2) {
+		if (baseclass = 0) {
+			HitAllocater = 140;
+		}
+		if (baseclass = 1) {
+			HitAllocater = 125;
+		}
+		if (baseclass = 2) {
+			HitAllocater = 105;
+		}
 		return HitAllocater;
 	}
 	else if (stat  = 3) {
+		if (baseclass = 0) {
+			AvoAllocater = 40;
+		}
+		if (baseclass = 1) {
+			AvoAllocater = 30;
+		}
+		if (baseclass = 2) {
+			AvoAllocater = 10;
+		}
 		return AvoAllocater;
 	}
 	else if (stat = 4) {
+		if (baseclass = 0) {
+			DefAllocater = 10;
+		}
+		if (baseclass = 1) {
+			DefAllocater = 12;
+		}
+		if (baseclass = 2) {
+			DefAllocater = 15;
+		}
 		return DefAllocater;
 	}
 	else if (stat = 5) {
+		if (baseclass = 0) {
+			MovAllocater = 6;
+		}
+		if (baseclass = 1) {
+			MovAllocater = 5;
+		}
+		if (baseclass = 2) {
+			MovAllocater = 5;
+		}
 		return MovAllocater;
 	}
 	else {
