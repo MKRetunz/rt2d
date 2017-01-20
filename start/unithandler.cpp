e4c08cd1ee6e701aa2bda1a042615324d11e1915
MKRetunz <Martijnklok@hotmail.nl>
2017-01-20 09:14:16 +0100

    Clarified which stat is which input in a comment

--- start/unithandler.cpp
+++ start/unithandler.cpp
@@ -14,9 +14,8 @@ UnitHandler::~UnitHandler()
 int UnitHandler::AllocateStats(int stat, int baseclass) {
 	int statAllocater = 0;
 
-	if (stat = 0) {
-		statAllocater = statSource->GetStat(stat, baseclass);
-	}
+	//HP = 0, Dam = 1, Hit = 2, Avo = 3, Def = 4, Mov = 5
+	statAllocater = statSource->GetStat(stat, baseclass);
 
 	return statAllocater;
 }
